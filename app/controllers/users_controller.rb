e0d222f4db51bf5f27aeb7f3543c5197dd68b57b
Manu Lohiya <manulohiya@gmail.com>
2015-08-10 17:39:13 -0700

    auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  def new
+
+  	@user = User.new
+  	render :new
+
+  end
+
+  def show
+  	@current_user = User.find(session[:user_id])
+  	render :show
+  end
+
+  def create
+  	# if current_user
+   #    redirect_to profile_path
+   #  else
+      user = User.new(user_params)
+  	   if user.save
+    		session[:user_id] = user.id
+    		redirect_to profile_path
+
+  		else
+  		redirect_to signup_path
+    	end
+  	# end
+  end
+
+private 
+  	def user_params
+  		params.require(:user).permit(:username, :email, :password)
+  	end	
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
- 
+
+  
 
  root "site#index"
+
+ #User routes
+ get "/signup", to: "users#new"
+ get "/profile", to: "users#show"
+
+ resources :users, only: [:create, :edit, :update]
+
+ # Sesions routes
+ get "/login", to: "sessions#new"
+ get "/logout", to: "sessions#destroy"
+ resources :sessions, only: [:create]
+ 
+
+
 
  
 end
