08715d3cfda1fc4657c7d82d6628b91338dad436
Sahash <sahash182@gmail.com>
2015-08-13 18:48:14 -0700

    new post button

--- db/migrate/20150813215224_update_tags.rb
+++
@@ -1,7 +0,0 @@
-class UpdateTags < ActiveRecord::Migration
-  def change
-  	add_reference :posts_tags, :post, index: true, foreign_key: true
-  	add_reference :posts_tags, :tag, index: true, foreign_key: true
-
-  end
-end
